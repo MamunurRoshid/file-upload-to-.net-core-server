90342de0521ea82fb75f57b64f8820cf76a23308
Mamunur <Mamunur@TVLLAP45>
2022-04-25 15:36:54 +0600

    ---

---
+++ WebApplication1/WebApplication1/FileController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    [Route("api/file")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55)
+            })
+            .ToArray();
+        }
+
+        [HttpPost("ImportFile")]
+        public async Task<IActionResult> ImportFile([FromForm] IFormFile file)
+        {
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/file", file.FileName);
+
+                using (Stream stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return StatusCode(200);
+            }
+            catch(Exception ex)
+            {
+                var dd = ex.Message;
+                return StatusCode(500);
+            }
+        }
+    }
+}
